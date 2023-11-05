61b8b48979ffa70178d3a12518de722e58ed6ed5
hgiudatto <hgiudatto@prismamp.com>
2023-11-05 10:06:34 -0300

    boosting fetchPooledRickAndMorty endpoint

--- src/promise-pool/promise-pool.service.ts
+++ src/promise-pool/promise-pool.service.ts
@@ -5,10 +5,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { AxiosError } from 'axios';
 import { PromisePool } from '@supercharge/promise-pool';
 import { firstValueFrom, catchError } from 'rxjs';
-import {
-  ReadRickAndMortyRequestDto,
-  ReadRickAndMortyResponseDto,
-} from 'src/promise-pool/promise-pool.dto';
+import { ReadRickAndMortyResponseDto } from 'src/promise-pool/promise-pool.dto';
 
 @Injectable()
 export class PromiseRickMortyPoolService {
@@ -16,31 +13,31 @@ export class PromiseRickMortyPoolService {
 
   constructor(private readonly httpService: HttpService) {}
 
-  retrieveRicks = async (rickId: string) => {
-    return new Promise(async (resolve, reject) => {
-      setTimeout(async () => {
-        const { data } = await firstValueFrom(
-          this.httpService
-            .get(`https://rickandmortyapi.com/api/character/${rickId}`)
-            .pipe(
-              catchError((error: AxiosError) => {
-                this.logger.error(error.response.data);
-                reject();
-                throw 'An error happened!';
-              }),
-            ),
-        );
-        resolve(data);
-      }, 5000);
-    });
-  };
-
   async fetchPooledRickAndMorty(
     userData,
   ): Promise<ReadRickAndMortyResponseDto> {
     const { rickMortyIds } = userData;
 
     const concurrency: number = 1;
+
+    const retrieveRicks = async (rickId: string) => {
+      return new Promise(async (resolve, reject) => {
+        setTimeout(async () => {
+          const { data } = await firstValueFrom(
+            this.httpService
+              .get(`https://rickandmortyapi.com/api/character/${rickId}`)
+              .pipe(
+                catchError((error: AxiosError) => {
+                  this.logger.error(error.response.data);
+                  reject();
+                  throw 'An error happened!';
+                }),
+              ),
+          );
+          resolve(data);
+        }, 5000);
+      });
+    };
 
     const { results, errors } = await PromisePool.for(rickMortyIds)
       .onTaskStarted((rick) => {
@@ -50,7 +47,7 @@ export class PromiseRickMortyPoolService {
         console.log(`Finaliza la busqueda del rick: ${rick}`);
       })
       .withConcurrency(concurrency)
-      .process(this.retrieveRicks);
+      .process(retrieveRicks);
 
     console.log('Results', results);
     console.log('Errors', errors);
